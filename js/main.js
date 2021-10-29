27f1938f5ea0a2dcee663a5feafeb260108b447d
Tolihama <fra070@gmail.com>
2021-10-29 16:17:53 +0200

    "Pari o dispari" script, "Palindroma" refactoring

--- js/main.js
+++ js/main.js
@@ -10,16 +10,27 @@ const palindromaOutput = document.getElementById('palindroma-output');
 // Palindroma script
 palindromaBtn.addEventListener('click', function() {
     // Data to check
-    const word = palindromaInput.value.trim();
-    const invertedWord = inverter(word);
+    const word = palindromaInput.value.trim().toLowerCase();
 
     // Check
-    if (word === invertedWord) {
+    if (palindromeCheck(word)) {
         palindromaOutput.innerHTML = '<span class="correct">La parola inserita è palindroma</span>';
     } else {
         palindromaOutput.innerHTML = '<span class="error">La parola inserita non è palindroma</span>';
     }
 });
+
+// FUNCTIONS
+// Palindrome check
+function palindromeCheck(word) {
+    let isPalindrome = false;
+    const wordToCheck = word;
+    const invertedWord = inverter(wordToCheck);
+    if (wordToCheck === invertedWord) {
+        isPalindrome = true;
+    }
+    return isPalindrome;
+}
 
 // Inverter function
 function inverter(word) {
@@ -39,9 +50,68 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
 // DOM Refs
-const btnEvenOdd = document.getElementById('even-odd-btn');
+const btnEvenOddScript = document.getElementById('even-odd-script-btn');
+const btnEven = document.getElementById('even-btn');
+const btnOdd = document.getElementById('odd-btn');
+const evenOddChoice = document.getElementById('even-odd-choice');
+const evenOddOutput = document.getElementById('even-odd-output');
+const evenOddResult = document.getElementById('even-odd-result');
+
+// Even or Odd user choice
+let evenUserChoice = false;
+btnEven.addEventListener('click', function() {
+    evenUserChoice = true;
+    evenOddChoice.innerHTML = "L'utente ha scelto 'pari'.";
+});
+btnOdd.addEventListener('click', function() {
+    evenUserChoice = false;
+    evenOddChoice.innerHTML = "L'utente ha scelto 'dispari'.";
+});
 
 // Script
-btnEvenOdd.addEventListener('click', function() {
+btnEvenOddScript.addEventListener('click', function() {
+    // User input
+    let userNumber = 0;
+    do {
+        userNumber = parseInt(prompt('Inserire un numero da 1 a 5.').trim());
+    } while (userNumber < 1 || userNumber > 5 || isNaN(userNumber))
 
+    // Computer number
+    const computerNumber = rand(1, 5);
+
+    // Sum
+    const sum = userNumber + computerNumber;
+
+    // Sum even or odd check
+    const sumIsEven = evenChecker(sum);
+
+    // Print output
+    evenOddOutput.innerHTML = `
+    <div>Numero scelto dall'utente: ${userNumber}</div>
+    <div>Numero del computer: ${computerNumber}</div>
+    <div>Somma: ${sum}</div>
+    `;
+
+    // User choice check
+    if (sumIsEven === evenUserChoice) {
+        evenOddResult.innerHTML = "L'utente ha vinto!";
+    } else {
+        evenOddResult.innerHTML = "Ha vinto il Computer!";
+    }
 });
+
+// FUNCTIONS
+// Rand function
+function rand(min, max) {
+    const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+    return rand;
+}
+
+// Even checker function
+function evenChecker(number) {
+    let isEven = false;
+    if (number % 2 === 0) {
+        isEven = true;
+    }
+    return isEven;
+}
