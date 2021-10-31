5c6ce6645db2cd84bcf4158d7b9885e91aa7923d
Tolihama <fra070@gmail.com>
2021-10-31 18:13:11 +0100

    Refactoring and graphic

--- js/main.js
+++ js/main.js
@@ -1,117 +1,146 @@
-/* Palindroma
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma */
-
-// DOM refs
-const palindromaInput = document.getElementById('palindroma-input');
-const palindromaBtn = document.getElementById('palindroma-btn');
-const palindromaOutput = document.getElementById('palindroma-output');
-
-// Palindroma script
-palindromaBtn.addEventListener('click', function() {
-    // Data to check
-    const word = palindromaInput.value.trim().toLowerCase();
-
-    // Check
-    if (palindromeCheck(word)) {
-        palindromaOutput.innerHTML = '<span class="correct">La parola inserita è palindroma</span>';
-    } else {
-        palindromaOutput.innerHTML = '<span class="error">La parola inserita non è palindroma</span>';
-    }
-});
-
-// FUNCTIONS
-// Palindrome check
-function palindromeCheck(word) {
-    let isPalindrome = false;
-    const wordToCheck = word;
-    const invertedWord = inverter(wordToCheck);
-    if (wordToCheck === invertedWord) {
-        isPalindrome = true;
-    }
-    return isPalindrome;
-}
-
-// Inverter function
-function inverter(word) {
-    let res = '';
-    for (let i = word.length - 1; i >= 0; i--) {
-        res += word[i];
-    }
-    return res;
-}
-
-
-/* Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto. */
-
-// DOM Refs
-const btnEvenOddScript = document.getElementById('even-odd-script-btn');
-const btnEven = document.getElementById('even-btn');
-const btnOdd = document.getElementById('odd-btn');
-const evenOddChoice = document.getElementById('even-odd-choice');
-const evenOddOutput = document.getElementById('even-odd-output');
-const evenOddResult = document.getElementById('even-odd-result');
-
-// Even or Odd user choice
-let evenUserChoice = false;
-btnEven.addEventListener('click', function() {
-    evenUserChoice = true;
-    evenOddChoice.innerHTML = "L'utente ha scelto 'pari'.";
-});
-btnOdd.addEventListener('click', function() {
-    evenUserChoice = false;
-    evenOddChoice.innerHTML = "L'utente ha scelto 'dispari'.";
-});
+// PALINDROMA
+// Dom refs
+const palInput = document.getElementById('palindroma-input');
+const palOutput = document.getElementById('palindroma-output');
+const palTrigger = document.getElementById('palindroma-btn');
 
 // Script
-btnEvenOddScript.addEventListener('click', function() {
-    // User input
-    let userNumber = 0;
-    do {
-        userNumber = parseInt(prompt('Inserire un numero da 1 a 5.').trim());
-    } while (userNumber < 1 || userNumber > 5 || isNaN(userNumber))
-
-    // Computer number
-    const computerNumber = rand(1, 5);
-
-    // Sum
-    const sum = userNumber + computerNumber;
-
-    // Sum even or odd check
-    const sumIsEven = evenChecker(sum);
-
-    // Print output
-    evenOddOutput.innerHTML = `
-    <div>Numero scelto dall'utente: ${userNumber}</div>
-    <div>Numero del computer: ${computerNumber}</div>
-    <div>Somma: ${sum}</div>
-    `;
-
-    // User choice check
-    if (sumIsEven === evenUserChoice) {
-        evenOddResult.innerHTML = "L'utente ha vinto!";
+palTrigger.addEventListener('click', function() {
+    const input = palInput.value.trim().toLowerCase();
+    let isPalindroma = false;
+    palOutput.classList.remove('d-none');
+    if (!specialCharCheck(input)) {
+        isPalindroma = palindromaCheck(input);
+        if (isPalindroma) {
+            palOutput.innerHTML = '<div class="text-success p-2">La parola è palindroma.</div>';
+        } else {
+            palOutput.innerHTML = '<div class="text-danger p-2">La parola non è palindroma.</div>';
+        }
     } else {
-        evenOddResult.innerHTML = "Ha vinto il Computer!";
+        palOutput.innerHTML = '<div class="text-warning p-2">Inserimento non valido. Non sono ammessi numeri e caratteri speciali.</div>';
     }
 });
 
-// FUNCTIONS
-// Rand function
-function rand(min, max) {
-    const rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    return rand;
+// Palindroma Functions
+function invertString(string) {
+    const stringToInvert = string;
+    let invertedString = '';
+    for (let i = stringToInvert.length - 1; i >= 0; i--) {
+        invertedString += stringToInvert[i];
+    }
+    return invertedString;
 }
 
-// Even checker function
-function evenChecker(number) {
-    let isEven = false;
-    if (number % 2 === 0) {
-        isEven = true;
+function specialCharCheck(string) {
+    const specialCharList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let thereIsSpecialChar = false;
+    for (let i = 0; i < string.length; i++) {
+        if (specialCharList.includes(string[i])) {
+            thereIsSpecialChar = true;
+        }
     }
-    return isEven;
+    return thereIsSpecialChar;
 }
+
+function palindromaCheck(string) {
+    const inputString = string;
+    const invertedString = invertString(inputString);
+    let isPalindroma = false;
+    if (inputString === invertedString) {
+        isPalindroma = true;
+    }
+    return isPalindroma;
+}
+
+// PARI E DISPARI
+// DOM refs
+const evenOddNumberInput = document.getElementById('even-odd-number-input');
+const evenRadio = document.getElementById('even');
+const oddRadio = document.getElementById('odd');
+const evenOddBtn = document.getElementById('even-or-odd-btn');
+const evenOddOutput = document.getElementById('even-or-odd-output');
+
+evenOddBtn.addEventListener('click', function() {
+    evenOddOutput.innerHTML = '';
+    evenOddOutput.classList.remove('d-none');
+    const userNumber = parseInt(evenOddNumberInput.value);
+    let userChoice = '';
+    if (evenRadio.checked) {
+        userChoice = 'even';
+    } else if (oddRadio.checked) {
+        userChoice = 'odd';
+    } else {
+        typingEffect(evenOddOutput, 'Selezionare "Pari" oppure "Dispari" per proseguire.', 50);
+        return;
+    }
+    if (userNumber < 1 || userNumber > 5) {
+        typingEffect(evenOddOutput, 'Si prega di selezionare un numero compreso tra 1 e 5', 50);
+        return;
+    } else {
+        let userChoiceIta = '';
+        let sumTypeIta = '';
+        let winner = '';
+        const pcNumber = rand(1, 5);
+        const sum = userNumber + pcNumber;
+        const sumType = oddOrEven(sum);
+        switch (userChoice) {
+            case 'odd':
+                userChoiceIta = 'dispari';
+                break;
+            case 'even':
+                userChoiceIta = 'pari';
+        }
+        switch (sumType) {
+            case 'odd':
+                sumTypeIta = 'dispari';
+                break;
+            case 'even':
+                sumTypeIta = 'pari';
+        }
+
+        userChoice === sumType ? winner = "L'utente ha vinto." : winner = 'Il computer ha vinto.';
+
+        const output = `
+        L'utente ha scelto ${userChoiceIta}.<br>
+        L'utente ha inserito ${userNumber}.<br>
+        Il computer ha generato il numero ${pcNumber}.<br>
+        La somma dei due numeri è ${sum} ed è un numero ${sumTypeIta}.<br>
+        ${winner}`;
+
+/*         typingEffect(evenOddOutput, output, 50); */
+        evenOddOutput.innerHTML = output;
+    }
+});
+
+// Functions
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function oddOrEven(number) {
+    let result = '';
+    number % 2 === 0 ? result = 'even' : result = 'odd';
+    return result;
+}
+
+
+// TYPING EFFECT
+/* function typingEffect(domElement, string, timeInterval) {
+    const stringToType = string;
+    let i = 0;
+    addChar();
+
+    function addChar() {
+        if (i < stringToType.length) {
+            domElement.innerHTML += stringToType[i];
+            i++;
+        } else {
+            clearTimeout(addChar);
+        }
+        setTimeout(addChar, timeInterval);
+    }
+}
+
+function addBreakRule(domElement) {
+    domElement.innerHTML += '<br>';
+} */
